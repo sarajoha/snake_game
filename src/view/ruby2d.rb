d797ea23e5e3f1b8b56a25ec8cb9c8e581d10fea
sarajoha <sjcc333@gmail.com>
2019-11-22 16:25:56 -0500

    snake moves

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -7,24 +7,28 @@ module View
       @pixel_size = 50
     end
 
-    def render(state)
+    def start(state)
       extend Ruby2D::DSL
       set(
         title: "Snake",
         width: @pixel_size * state.grid.cols,
         height: @pixel_size * state.grid.rows)
+      show
+    end
+
+    def render(state)
       render_snake(state)
       render_food(state)
-      show
     end
 
     private
 
     def render_food(state)
+      @food.remove if @food
       extend Ruby2D::DSL
       food = state.food
       # byebug
-      Square.new(
+      @food = Square.new(
         x: food.col * @pixel_size,
         y: food.row * @pixel_size,
         size: @pixel_size,
@@ -33,10 +37,11 @@ module View
     end
 
     def render_snake(state)
+      @snake_positions.each(&:remove) if @snake_positions
       extend Ruby2D::DSL
       snake = state.snake
       # byebug
-      snake.positions.each do |pos|
+      @snake_positions = snake.positions.map do |pos|
       Square.new(
         x: pos.col * @pixel_size,
         y: pos.row * @pixel_size,
